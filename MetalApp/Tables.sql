d65f744875aa29e7b0d51905fa58ff32a7924a27
sdmoralesma <sdmoralesma@gmail.com>
2013-03-17 00:14:46 -0500

    Mejorado SQL file

--- MetalApp/Tables.sql
+++ MetalApp/Tables.sql
@@ -1,6 +1,6 @@
---Precaution: First create the database from a SQL Console
---CREATE DATABASE IF NOT EXISTS metal;
---USE metal;
+-- Precaution: First create the database from a SQL Console
+-- CREATE DATABASE IF NOT EXISTS metal;
+-- USE metal;
 
 DROP TABLE IF EXISTS `metal`.`jury`;
 DROP TABLE IF EXISTS `metal`.`presentation`;
@@ -14,14 +14,16 @@ DROP TABLE IF EXISTS `metal`.`score_matrix`;
 DROP TABLE IF EXISTS `metal`.`users`;
 DROP TABLE IF EXISTS `metal`.`song_matrix`;
 
+-- =====================
 -- security Realm Tables
+-- =====================
+
 CREATE TABLE `metal`.`groups` (
 	`id_group` VARCHAR(50),
 	`username` VARCHAR(50)
 ) ENGINE=InnoDB;
 
-ALTER TABLE groups ADD CONSTRAINT GroupsRealm_PK PRIMARY KEY
-(
+ALTER TABLE groups ADD CONSTRAINT GroupsRealm_PK PRIMARY KEY (
   username
 );
 
@@ -30,58 +32,52 @@ CREATE TABLE `metal`.`users` (
 	`password` VARCHAR(100)
 ) ENGINE=InnoDB;
 
-ALTER TABLE users ADD CONSTRAINT UsersRealm_PK PRIMARY KEY
-(
+ALTER TABLE users ADD CONSTRAINT UsersRealm_PK PRIMARY KEY (
   username
 );
 
+
+-- =============
 -- Domain Tables
-CREATE TABLE artist
-(
+-- =============
+
+CREATE TABLE artist (
 	id_artist INTEGER NOT NULL ,
     name      VARCHAR (50) NOT NULL
-) ;
-ALTER TABLE artist ADD CONSTRAINT Artist_PK PRIMARY KEY
-(
+);
+ALTER TABLE artist ADD CONSTRAINT Artist_PK PRIMARY KEY (
   id_artist
-)
-;
-CREATE TABLE gender
-  (
-    id_gender  INTEGER NOT NULL ,
+);
+
+CREATE TABLE gender (
+	id_gender  INTEGER NOT NULL ,
     name       VARCHAR (50) NOT NULL ,
     hang_value INTEGER NOT NULL ,
     head_value INTEGER NOT NULL
-  ) ;
-ALTER TABLE gender ADD CONSTRAINT Gender_PK PRIMARY KEY
-(
+);
+ALTER TABLE gender ADD CONSTRAINT Gender_PK PRIMARY KEY (
   id_gender
-)
-;
-CREATE TABLE jury
-  (
-    id_jury  INTEGER NOT NULL ,
+);
+
+CREATE TABLE jury (
+	id_jury  INTEGER NOT NULL ,
     name     VARCHAR (100) NOT NULL ,
     username VARCHAR (50) NOT NULL ,
     password VARCHAR (50) NOT NULL
-  ) ;
-ALTER TABLE jury ADD CONSTRAINT Jury_PK PRIMARY KEY
-(
+);
+ALTER TABLE jury ADD CONSTRAINT Jury_PK PRIMARY KEY (
   id_jury
-)
-;
-CREATE TABLE jury_presentation
-  (
-    presentation_id_presentation INTEGER NOT NULL ,
+);
+
+CREATE TABLE jury_presentation (
+	presentation_id_presentation INTEGER NOT NULL ,
     jury_id_jury                 INTEGER NOT NULL
-  ) ;
-ALTER TABLE jury_presentation ADD CONSTRAINT jury_presentation__IDX PRIMARY KEY
-(
+);
+ALTER TABLE jury_presentation ADD CONSTRAINT jury_presentation__IDX PRIMARY KEY (
   presentation_id_presentation, jury_id_jury
-)
-;
-CREATE TABLE participant
-  (
+);
+
+CREATE TABLE participant (
     id_participat INTEGER NOT NULL ,
     name          VARCHAR (100) NOT NULL ,
     phone         INTEGER NOT NULL ,
@@ -89,14 +85,12 @@ CREATE TABLE participant
     gender        VARCHAR (50) NOT NULL ,
     username      VARCHAR (50) NOT NULL ,
     password      VARCHAR (50) NOT NULL
-  ) ;
-ALTER TABLE participant ADD CONSTRAINT Participant_PK PRIMARY KEY
-(
+);
+ALTER TABLE participant ADD CONSTRAINT Participant_PK PRIMARY KEY (
   id_participat
-)
-;
-CREATE TABLE presentation
-  (
+);
+
+CREATE TABLE presentation (
     id_presentation INTEGER NOT NULL ,
     id_participant  INTEGER NOT NULL ,
     id_song         INTEGER NOT NULL ,
@@ -106,14 +100,12 @@ CREATE TABLE presentation
     total_score FLOAT NOT NULL ,
     id_participat INTEGER ,
     id_song1      INTEGER
-  ) ;
-ALTER TABLE presentation ADD CONSTRAINT Prsentation_PK PRIMARY KEY
-(
+);
+ALTER TABLE presentation ADD CONSTRAINT Prsentation_PK PRIMARY KEY (
   id_presentation
-)
-;
-CREATE TABLE score_matrix
-  (
+);
+
+CREATE TABLE score_matrix (
     id_participant INTEGER NOT NULL ,
     head1          INTEGER ,
     head2          INTEGER ,
@@ -139,28 +131,24 @@ CREATE TABLE score_matrix
     head_score FLOAT ,
     total_score FLOAT ,
     id_participat INTEGER
-  ) ;
-ALTER TABLE score_matrix ADD CONSTRAINT Score_Matriz_PK PRIMARY KEY
-(
+);
+ALTER TABLE score_matrix ADD CONSTRAINT Score_Matriz_PK PRIMARY KEY (
   id_participant
-)
-;
-CREATE TABLE song
-  (
+);
+
+CREATE TABLE song (
     id_song    INTEGER NOT NULL ,
     title      VARCHAR (100) NOT NULL ,
     id_artist  INTEGER NOT NULL ,
     id_gender  INTEGER NOT NULL ,
     id_artist1 INTEGER ,
     id_gender1 INTEGER
-  ) ;
-ALTER TABLE song ADD CONSTRAINT Song_PK PRIMARY KEY
-(
+);
+ALTER TABLE song ADD CONSTRAINT Song_PK PRIMARY KEY (
   id_song
-)
-;
-CREATE TABLE song_matrix
-  (
+);
+
+CREATE TABLE song_matrix (
     id_song       INTEGER NOT NULL ,
     musicality1   INTEGER ,
     musicality2   INTEGER ,
@@ -186,18 +174,14 @@ CREATE TABLE song_matrix
     composition_score FLOAT ,
     total_score FLOAT ,
     id_song1 INTEGER
-  ) ;
-ALTER TABLE song_matrix ADD CONSTRAINT Song_Matriz_PK PRIMARY KEY
-(
+);
+ALTER TABLE song_matrix ADD CONSTRAINT Song_Matriz_PK PRIMARY KEY (
   id_song
-)
-;
-ALTER TABLE jury_presentation ADD CONSTRAINT FK_ASS_26 FOREIGN KEY
-(
+);
+
+ALTER TABLE jury_presentation ADD CONSTRAINT FK_ASS_26 FOREIGN KEY (
   presentation_id_presentation
-)
-REFERENCES presentation
-(
+) REFERENCES presentation (
   id_presentation
 )
 ON
