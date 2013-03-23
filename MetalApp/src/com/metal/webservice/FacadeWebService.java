1fb07506887d88d0e99a232104086f65218abac5
sdmoralesma <sdmoralesma@gmail.com>
2013-03-23 07:34:54 -0500

    Agregada infraestructura Web service soap

--- MetalApp/src/com/metal/webservice/FacadeWebService.java
+++ MetalApp/src/com/metal/webservice/FacadeWebService.java
@@ -1,20 +1,32 @@
 package com.metal.webservice;
 
+import java.util.List;
+
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+
+import com.metal.model.Participant;
+import com.metal.model.Song;
 
 /**
  * Session Bean implementation class WebServiceFacade
  */
+@WebService
 @Stateless
 @LocalBean
 public class FacadeWebService {
 
-    /**
-     * Default constructor. 
-     */
-    public FacadeWebService() {
-        // TODO Auto-generated constructor stub
-    }
+	public List<Participant> getRatingParticipants() {
+		return null;
+	}
 
+	public List<Song> getRatingSongs() {
+		return null;
+	}
+
+	public Participant getRatingPerParticipant(@WebParam Participant participant) {
+		return null;
+	}
 }
